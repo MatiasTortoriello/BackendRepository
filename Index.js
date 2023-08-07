8b0067fedb441ec5208b88f7bde22793d018201d
MatiasTortoriello <matiastortoriello1991@gmail.com>
2023-08-07 14:11:54 -0300

    .

--- Index.js
+++ Index.js
@@ -1,24 +1,22 @@
-import ProductManger from './src/ProductMananger.js';
-import express from 'express';
+import ProductManger from '../ProductMananger.js';
+import {Router} from 'express';
 
-const app = express();
-app.use(express.json());
+const router = Router();
 
-app.use(express.urlencoded({ extended: true }));
 
-const productOne = new ProductManger("./products.json");
+const productManager = new ProductManger("./products.json");
 
 
 
-app.get('/products', async (req, res) => {
+router.get('/products', async (req, res) => {
 
     const limit = parseInt(req.query.limit);
 
-    const products = await productOne.sendProducts();
+    const products = await productManager.sendProducts();
 
     if (!limit || !limit == Number) {
 
-        return res.send({ products })
+        return res.send({ productsSended: products })
     }
 
     const limitProducts = products.slice(0, limit);
@@ -30,21 +28,60 @@ app.get('/products', async (req, res) => {
 
 
 
-app.get('/products/:pid', async (req,res)=>{
+router.get('/:id', async (req, res) => {
 
-    const idProduct=parseInt(req.params.pid);
+    const idProduct = parseInt(req.params.id);
 
-    const products = await productOne.sendProducts();
+    const productid = await productManager.getProductsById(idProduct);
 
-    let product=products.find(prod=>prod.id == idProduct);
+    if (!productid) return (res.status(400).send({ error: "Product no founded" }))
 
-    if(!product) return (res.send({error:"Product no founded"}))
+    return res.send({ productFinded: productid })
 
-    return res.send({productFinded:product})
-
-    }
+}
 );
 
+
+router.post('/', async(req,res)=>{
+
+    const products = await productManager.sendProducts();
+    
+    const productAdded=req.body;
+    
+    const productDouble= await products.find(prod=>prod.code === productAdded.code);
+    
+    if(productDouble){
+        return res.status(400).send({error:"El producto que intenta agregar ya ha sido creado"})
+}
+    
+    console.log(productDouble);
+        res.send(await productManager.addProduct(req.body));
+});
+
+router.put('/:id', async (req,res)=>{
+
+    const idProduct = parseInt(req.params.id);
+    
+    res.send(await productManager.updateProduct(idProduct,req.body));
+});
+    
+
+    
+router.delete('/:id', async (req,res)=>{
+    
+    const products = await prodManger.sendProducts();
+    
+    const idProduct = parseInt(req.params.id);
+    
+    const confirmDelete=products.find(prod=>prod.id == idProduct)
+    
+    if(!confirmDelete) return res.status(400).send({error:"product not found"})
+    
+    res.send(await productManager.deletProduct(idProduct));
+})
+    
+    
+export default router;
 
 
 const PORT = 8080;
